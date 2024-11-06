2830ed25db21658370bcf509bae78134a44a314d
Sardorbek Akhmedov <110537414+sardorbekakhmedov@users.noreply.github.com>
2024-11-06 18:25:48 +0500

    fix

---
+++ Program.cs
@@ -0,0 +1,60 @@
+using System.Reflection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using ElasticsearchSinkOptions = Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions;
+
+var builder = WebApplication.CreateBuilder(args);
+ConfigureLogging();
+builder.Host.UseSerilog();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.UseHttpsRedirection();
+
+app.Run();
+return;
+
+void ConfigureLogging()
+{
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{environment}.json", optional: true)
+        .Build();
+
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails()
+        .WriteTo.Debug()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+        .Enrich.WithProperty("Environment", environment)
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
+
+ElasticsearchSinkOptions ConfigureElasticSink(IConfiguration configuration, string environment)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name?.ToLower().Replace(".","-")}-{environment.ToLower()}-{DateTime.Now:yyyy-MM}",
+        MinimumLogEventLevel = LogEventLevel.Information,
+        NumberOfReplicas = 2,
+        NumberOfShards = 2
+    };
+}
+
