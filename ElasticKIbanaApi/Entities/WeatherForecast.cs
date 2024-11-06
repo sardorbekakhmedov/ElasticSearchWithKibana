8ad3030868377f907fa8024296559b65334ce7e4
Sardorbek Akhmedov <110537414+sardorbekakhmedov@users.noreply.github.com>
2024-11-06 18:17:37 +0500

    first commit

---
+++ ElasticKIbanaApi/Entities/WeatherForecast.cs
@@ -0,0 +1,9 @@
+﻿namespace ElasticKIbanaApi.Entities;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+    public int  TemperatureC { get; set; }
+    public int  TemperatureF { get; set; }
+    public string Summary { get; set; }
+}
