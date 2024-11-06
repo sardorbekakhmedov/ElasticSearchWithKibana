13094c8b4082ab632b849f45764bb286b3d8f56e
Sardorbek Akhmedov <110537414+sardorbekakhmedov@users.noreply.github.com>
2024-11-06 18:24:32 +0500

    minor fix

--- ElasticKIbanaApi/Entities/WeatherForecast.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ElasticKIbanaApi.Entities;
-
-public class WeatherForecast
-{
-    public DateOnly Date { get; set; }
-    public int  TemperatureC { get; set; }
-    public int  TemperatureF { get; set; }
-    public string Summary { get; set; }
-}
